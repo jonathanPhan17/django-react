24e432f4f5fd5ed2d8dfad6ff9925c3cedb539bf
jonathanPhan17 <shoxie1999@gmail.com>
2024-08-10 18:17:28 -0700

    Add Form component for login and registration

--- frontend/src/pages/NotFound.jsx
+++ frontend/src/pages/NotFound.jsx
@@ -1,6 +1,9 @@
 function NotFound() {
     return (
-        <div>NotFound</div>
+        <div>
+            <h1>404 Not Found.</h1>
+            <p>The page you are looking for doesn't exist.</p>
+        </div>
     )
 }
 
